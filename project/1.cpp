56fe58921619b05d781ae8b69b159b8cab6e19ac
“wangsong” <1665834268@qq.com>
2022-01-26 02:15:25 +0800

    master

---
+++ project/1.cpp
@@ -0,0 +1,17 @@
+#include<iostream> 
+
+using namespace std; 
+int list(void);
+int main(void)
+{
+    cout << "你好世界,你好" << endl;
+    list();
+    return 0;
+}
+int list(void)
+{
+    cout << "this is hansu\n";
+    return 0;  
+
+}
+
---
+++ project/2.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+using namespace std;
+
+int main(void)
+{   
+    int sum;
+    cout << "you are pig\n";
+    cin >> sum ;
+    cout << "input sum : ";
+    cout << sum;
+    return 0 ;
+
+
+}
