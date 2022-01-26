98f6f023d16fcd62e89f88f5c1a80868703acbcd
Auroraol <1665834268@qq.com>
2022-01-26 18:45:35 +0800

    master

---
+++ project/8.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+int main(void)
+{
+    using namespace std;
+    cout << "Eeter ";
+    return 0;
+
+}
---
+++ project/9.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+int main(void)
+{
+    using namespace std;
+    cout << "enter :\n";
+    cout << "first" << endl;
+
+
+    return 0 ;
+
+
+
+
+}
