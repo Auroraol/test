f4a2a9d40f18b4d538c89f7d1152be08554b11e5
Auroraol <1665834268@qq.com>
2022-01-27 10:30:20 +0800

    master

--- project/9.cpp
+++ project/9.cpp
@@ -1,15 +1,27 @@
-#include<iostream>
+// #include<iostream>
 
+// int main(void)
+// {
+//     using namespace std;
+//     cout << "enter :\n";
+//     cout << "first" << endl;
+
+
+//     return 0 ;
+
+
+
+
+// }
+
+#include<iostream>
+using namespace std;
 int main(void)
 {
-    using namespace std;
-    cout << "enter :\n";
-    cout << "first" << endl;
-
-
-    return 0 ;
-
-
-
-
+    const int a = 7;
+    char think[a];
+    cout << "plaese input your think: /n";
+    cin >> think;
+    cout << "your think is: " << think << endl;
+    return 0;
 }
